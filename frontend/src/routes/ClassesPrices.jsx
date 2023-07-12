00fd732b43784d7f70c1ba0a6fee3be4fe46ee41
brandonscott4 <bscott15@sky.com>
2023-07-12 13:19:14 +0100

    added content to classes & prices section

--- frontend/src/routes/ClassesPrices.jsx
+++ frontend/src/routes/ClassesPrices.jsx
@@ -7,16 +7,37 @@ function ClassesPrices() {
         </h2>
         <div className="flex flex-col justify-center items-center">
           <div className="flex flex-col md:flex-row gap-10 mx-5 items-center md:items-stretch">
-            <img src="/googlemap.PNG" alt="map" className=" w-4/5 md:w-1/2" />
+            <img src="/googlemap.PNG" alt="map" className=" w-4/5 md:w-3/5" />
             <div className="bg-white rounded-2xl w-4/5 md:w-1/3 shadow-xl flex flex-col items-center justify-center">
               <div className="text-left p-10 text-lg font-medium md:text-xl">
                 <p>Random Sports Centre</p>
                 <p>Dartford</p>
                 <p>100 Street Road</p>
                 <p>DA1 8NG</p>
+                <div className="divider"></div>
+                <p className="mt-8">
+                  <b>Thursdays: </b>12:30pm - 1:30pm
+                </p>
+                <p className="mt-4">
+                  <b>Saturdays: </b>11:30pm - 12:30pm
+                </p>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="py-20 w-full flex gap-24 justify-center items-center">
+        <div className="w-1/6 text-center">
+          <h3 className="font-semibold text-lg">Kids classes (5-17)</h3>
+          <div className="divider"></div>
+          <p>£13 per hour</p>
+        </div>
+
+        <div className="w-1/6 text-center">
+          <h3 className="font-semibold text-lg">Adults classes (18+)</h3>
+          <div className="divider"></div>
+          <p>£18 per hour</p>
         </div>
       </div>
     </>
