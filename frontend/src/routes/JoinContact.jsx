f546035a3592b737ce697e7679819b584910a7bc
brandonscott4 <bscott15@sky.com>
2023-09-14 00:49:30 +0100

    added form validation

--- frontend/src/routes/JoinContact.jsx
+++ frontend/src/routes/JoinContact.jsx
@@ -1,11 +1,114 @@
+import { useEffect, useState } from "react";
+
 function JoinContact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNo, setMobileNo] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [formError, setFormError] = useState({
+    name: false,
+    email: false,
+    mobileNo: false,
+    message: false,
+    errorMsg: {
+      error: false,
+      msg: "",
+    },
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+
+    if (id === "name") {
+      setName(value);
+    } else if (id === "email") {
+      setEmail(value);
+    } else if (id === "mobileNo") {
+      setMobileNo(value);
+    } else {
+      setMessage(value);
+    }
+
+    setFormError((prevFormError) => ({
+      ...prevFormError,
+      [id]: false,
+    }));
+  };
+
+  useEffect(() => {
+    if (formError.errorMsg.error) {
+      if (
+        !formError.name &&
+        !formError.email &&
+        !formError.mobileNo &&
+        !formError.message
+      ) {
+        setFormError((prevFormError) => ({
+          ...prevFormError,
+          errorMsg: {
+            error: false,
+            msg: "",
+          },
+        }));
+      }
+    } else {
+      if (
+        formError.name ||
+        formError.email ||
+        formError.mobileNo ||
+        formError.message
+      ) {
+        setFormError((prevFormError) => ({
+          ...prevFormError,
+          errorMsg: {
+            error: true,
+            msg: "Please enter the required field(s)",
+          },
+        }));
+      }
+    }
+  }, [formError]);
+
+  const handleSubmit = (e) => {
+    if (name === "") {
+      setFormError((prevFormError) => ({
+        ...prevFormError,
+        name: true,
+      }));
+    }
+    if (email === "") {
+      setFormError((prevFormError) => ({
+        ...prevFormError,
+        email: true,
+      }));
+    }
+    if (mobileNo === "") {
+      setFormError((prevFormError) => ({
+        ...prevFormError,
+        mobileNo: true,
+      }));
+    }
+    if (message === "") {
+      setFormError((prevFormError) => ({
+        ...prevFormError,
+        message: true,
+      }));
+    }
+
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className=" py-20 w-full bg-sky-100 flex flex-col justify-center items-center">
         <h2 className="text-4xl mb-12 font-semibold underline">
           Get in contact
         </h2>
-        <form className="bg-white w-4/5 sm:w-3/4 lg:w-1/2 2xl:w-2/6 mx-8 px-8 sm:px-20 py-12 shadow-xl rounded-2xl">
+        <form
+          className="bg-white w-4/5 sm:w-3/4 lg:w-1/2 2xl:w-2/6 mx-8 px-8 sm:px-20 py-12 shadow-xl rounded-2xl"
+          onSubmit={handleSubmit}
+        >
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="name"
@@ -13,10 +116,14 @@ function JoinContact() {
             Name
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              formError.name ? "border-red-500" : ""
+            }`}
             id="name"
             type="text"
             placeholder="Jane Doe"
+            value={name}
+            onChange={handleChange}
           ></input>
 
           <label
@@ -26,23 +133,31 @@ function JoinContact() {
             Email
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              formError.email ? "border-red-500" : ""
+            }`}
             id="email"
             type="email"
             placeholder="janedoe@gmail.com"
+            value={email}
+            onChange={handleChange}
           ></input>
 
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-6"
-            htmlFor="mobile"
+            htmlFor="mobileNo"
           >
             Mobile
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
-            id="mobile"
-            type="email"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              formError.mobileNo ? "border-red-500" : ""
+            }`}
+            id="mobileNo"
+            type="tel"
             placeholder="07123456789"
+            value={mobileNo}
+            onChange={handleChange}
           ></input>
 
           <label
@@ -51,7 +166,21 @@ function JoinContact() {
           >
             Message
           </label>
-          <textarea className="resize-y shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none h-40"></textarea>
+          <textarea
+            id="message"
+            value={message}
+            onChange={handleChange}
+            className={`resize-y shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none h-40 ${
+              formError.message ? "border-red-500" : ""
+            } `}
+          ></textarea>
+          <p
+            className={`text-red-500 font-semibold mt-3${
+              formError.errorMsg.error ? "" : "hidden"
+            }`}
+          >
+            {formError.errorMsg.msg}
+          </p>
           <button className="btn btn-neutral btn-xs sm:btn-sm md:btn-md mt-8">
             Submit
           </button>
